674391001f75de0f5f7048f55cd2c1435583c75b
GarlicMachine <jhn153228@gmail.com>
2021-04-14 11:27:48 +0900

    AWS 적용

--- src/oracle/dbconfig.js
+++ src/oracle/dbconfig.js
@@ -1,7 +1,6 @@
 module.exports =
 {
-    user: process.env.MODE_ORACLEDB_USER || "teamProject",
-    password: process.env.MODE_ORACLEDB_PASSWORD || "tiger",
-    connectString : process.env.MODE_ORACLEDB_CONNECTIONSTRING || "192.168.0.39/xe"
-
+    user: process.env.MODE_ORACLEDB_USER || "admin",
+    password: process.env.MODE_ORACLEDB_PASSWORD || "1q2w3e4r",
+    connectString : process.env.MODE_ORACLEDB_CONNECTIONSTRING || "admin.cispvxsijups.ap-northeast-2.rds.amazonaws.com/orcl" 
 }
